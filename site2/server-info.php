e098e2a0a1f170e16264d6fb6493d56f91c78ca3
Conrad Knapp <knappco@bc.edu>
2018-04-27 21:32:10 -0400

    Finishes site 2

--- site2/server-info.php
+++ site2/server-info.php
@@ -1,3 +1,13 @@
 <?php 
-  # SERVER INFO
+  $server = [
+    'Host Server Name' => $_SERVER['SERVER_NAME'],
+    'Server Software' => $_SERVER['SERVER_SOFTWARE'],
+    'Current Page' => $_SERVER['PHP_SELF'],
+    'Absolute Path' => $_SERVER['SCRIPT_FILENAME']
+  ];
+
+  $client = [
+    'Client System Info' => $_SERVER['HTTP_USER_AGENT']
+  ];
+
 ?>
