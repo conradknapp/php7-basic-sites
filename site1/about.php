24dc6fccb309d96b7d6a27b3dbcc2e99ab49ac9d
Conrad Knapp <conradknapp@Conrads-MBP.columbus.rr.com>
2018-04-27 21:02:11 -0400

    Initial commit

---
+++ site1/about.php
@@ -0,0 +1,4 @@
+<?php include 'include/header.php'; ?>
+
+<h2>About</h2>
+<?php include 'include/footer.php'; ?>
---
+++ site1/contact.php
@@ -0,0 +1,4 @@
+<?php include 'include/header.php'; ?>
+
+<h2>Contact</h2>
+<?php include 'include/footer.php'; ?>
